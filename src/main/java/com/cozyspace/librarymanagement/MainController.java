12a0f6ba54616e580f74fea47d4fa6efe213387d
homulily85 <nguyenkimtrungduc2005@gmail.com>
2024-10-07 17:28:12 +0700

    Add some logic for login screen

--- src/main/java/com/cozyspace/librarymanagement/MainController.java
+++ src/main/java/com/cozyspace/librarymanagement/MainController.java
@@ -1,4 +1,30 @@
 package com.cozyspace.librarymanagement;
 
+import com.cozyspace.librarymanagement.user.Account;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class MainController {
+
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField idField;
+
+    Account admin = new Account("admin", "admin");
+
+    public boolean validateAccount() {
+        if (idField.getText().equals(admin.getId())
+                && passwordField.getText().equals(admin.getPassword())) {
+            System.out.println("Correct");
+            return true;
+        } else {
+            System.out.println("Incorrect");
+            return false;
+        }
+    }
+
 }
