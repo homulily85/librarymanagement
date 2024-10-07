8aeb581800697401f4dc85a105e2b25ed07a3903
homulily85 <nguyenkimtrungduc2005@gmail.com>
2024-10-07 23:36:27 +0700

    Add error dialog for failed authenticity

--- src/main/java/com/cozyspace/librarymanagement/MainController.java
+++ src/main/java/com/cozyspace/librarymanagement/MainController.java
@@ -1,14 +1,21 @@
 package com.cozyspace.librarymanagement;
 
 import com.cozyspace.librarymanagement.user.Account;
+import javafx.beans.binding.Binding;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MainController {
-
+    @FXML
+    private Button loginButton;
+    @FXML
+    private VBox loginVbox;
     @FXML
     private PasswordField passwordField;
     @FXML
@@ -16,14 +23,37 @@ public class MainController {
 
     Account admin = new Account("admin", "admin");
 
-    public boolean validateAccount() {
-        if (idField.getText().equals(admin.getId())
-                && passwordField.getText().equals(admin.getPassword())) {
-            System.out.println("Correct");
-            return true;
-        } else {
-            System.out.println("Incorrect");
-            return false;
+    public void initialize() {
+        // Tắt nút đăng nhăp khi trường tên đăng nhập và mật khẩu rỗng
+        loginButton.disableProperty()
+                .bind(Bindings.isEmpty(idField.textProperty()).or(passwordField.textProperty().isEmpty()));
+    }
+
+    /**
+     * Kiểm tra thông tin đăng nhập của người dùng.
+     * Phương thức được gọi khi người dúng ấn vào nút "Đăng nhập".
+     *
+     * @return true nếu thông tin khớp với một tài khoản trong CSDL
+     */
+    private boolean validateAccount() {
+        return idField.getText().equals(admin.getId())
+                && passwordField.getText().equals(admin.getPassword());
+    }
+
+    public void handleLoginAction() {
+        if (!validateAccount()) {
+            Dialog<ButtonType> dialog = new Dialog<>();
+            dialog.initOwner(loginVbox.getScene().getWindow());
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(Objects.requireNonNull(getClass().getResource("authenticity_fail_dialog.fxml")));
+            try {
+                dialog.getDialogPane().setContent(fxmlLoader.load());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            dialog.setTitle("Lỗi");
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+            dialog.showAndWait();
         }
     }
 
