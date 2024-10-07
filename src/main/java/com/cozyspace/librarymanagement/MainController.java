782cc795fb066207fad62e187b10f9e0c8fb4dcd
homulily85 <nguyenkimtrungduc2005@gmail.com>
2024-10-07 15:46:05 +0700

    Edit project structure

--- src/main/java/com/cozyspace/librarymanagement/MainController.java
+++ src/main/java/com/cozyspace/librarymanagement/MainController.java
@@ -3,7 +3,7 @@ package com.cozyspace.librarymanagement;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
